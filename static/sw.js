981415a9061a4f3d802698a6cfeb761f684bb0bc
tuhin118 <tuhin432001@gmail.com>
2026-08-21 19:25:44 +0600

    Update service worker cache and file handling

--- static/sw.js
+++ static/sw.js
@@ -1,40 +1,40 @@
-const CACHE_NAME = "tuhin-image-lab-v1";
+const CACHE_NAME = "tuhin-image-finder-v2";
 
-const APP_FILES = [
-    "/",
-    "/static/manifest.json"
+const FILES_TO_CACHE = [
+  "/",
+  "/static/manifest.json",
+  "/static/icon-192.png",
+  "/static/icon-512.png"
 ];
 
 self.addEventListener("install", event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(APP_FILES))
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
 
-    self.skipWaiting();
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
-    event.waitUntil(
-        caches.keys().then(keys =>
-            Promise.all(
-                keys
-                    .filter(key => key !== CACHE_NAME)
-                    .map(key => caches.delete(key))
-            )
-        )
-    );
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
 
-    self.clients.claim();
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
-    if (event.request.method !== "GET") {
-        return;
-    }
-
-    event.respondWith(
-        fetch(event.request)
-            .catch(() => caches.match(event.request))
-    );
+  event.respondWith(
+    fetch(event.request).catch(() =>
+      caches.match(event.request)
+    )
+  );
 });
